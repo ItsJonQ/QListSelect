ad2c1a71b905149fabf16318b605574a7174354c
ItsJonQ <itsjonq@gmail.com>
2013-09-12 00:06:54 -0400

    Added function and logic for keyboard based selection for next/prev

--- assets/js/jquery.qlistselect.js
+++ assets/js/jquery.qlistselect.js
@@ -9,6 +9,7 @@
 			qLS.select = qLS.li.hasClass(qLS.selectClass);
 
 			var settings = $.extend({
+				initialFocus		: true,
 				keyboardActions		: true
 			});
 
@@ -17,10 +18,25 @@
 				ele.addClass(qLS.selectClass).focus();
 			};
 
-			qLS.actionSelectNext = function(ele) {
-				qLS.t.find('li:focus').removeClass(qLS.selectClass);
-				qLS.t.find('li:focus').next().focus().addClass(qLS.selectClass);
-			}
+			qLS.actionSelectFocus = function(direction) {
+				var ele = qLS.t.find('li:focus');
+				ele.removeClass(qLS.selectClass);
+				if(direction === 'next') {
+					if(!ele.is(':last-child')) {
+						ele.next().focus().addClass(qLS.selectClass);
+					} else {
+						ele.addClass(qLS.selectClass);
+					}
+				} else if (direction === 'prev' || direction === 'previous' ) {
+					if(!ele.is(':first-child')) {
+						ele.prev().focus().addClass(qLS.selectClass);
+					} else {
+						ele.addClass(qLS.selectClass);
+					}
+				} else {
+					return false;
+				}
+			};
 
 			// Adding TabIndex to List Items
 			var liCount = 0;
@@ -29,13 +45,21 @@
 				$(this).attr('tabindex', liCount);
 			});
 
+			// Settings: Initial Focus
+			if(settings.initialFocus) {
+				qLS.li.first().focus();
+			}
+
 			// Settings: Keyboard Actions
 			if(settings.keyboardActions) {
 				$(document).on('keydown', function(e) {
-					if(e.keyCode === 9) {
+					if(e.keyCode === 38 || e.keyCode === 87) {
 						e.preventDefault();
-						console.log('Tab is Pressed');
-						qLS.actionSelectNext();
+						qLS.actionSelectFocus('prev');
+					}
+					if(e.keyCode === 9 || e.keyCode === 40 || e.keyCode === 83) {
+						e.preventDefault();
+						qLS.actionSelectFocus('next');
 					}
 				});				
 			}
